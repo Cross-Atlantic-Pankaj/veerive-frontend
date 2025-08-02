1ee133b555e8deae237dbfbd013683ece36457a6
Sohail Belim <sohailbelim2425@kgpian.iitkgp.ac.in>
2025-08-03 02:24:56 +0530

    One conditional rendering in context details

--- src/app/(authenticated)/context-details/[slug]/_components/TrendingExpertOpinion.js
+++ src/app/(authenticated)/context-details/[slug]/_components/TrendingExpertOpinion.js
@@ -1,20 +1,25 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
 
-const TrendingExpertOpinion = ({ trendingExpertOpinions }) => {
-	if (!trendingExpertOpinions || trendingExpertOpinions.length === 0) {
-		return null;
-	}
-
-	return (
-		<div className="bg-white mt-4 p-4 sm:p-5 rounded-xl shadow-md">
-			<div className="flex justify-between items-center mb-4">
-				<h2 className="font-semibold text-base sm:text-lg text-gray-900">
+const TrendingThemes = ({ trendingExpertOpinions }) => {
+  return (
+    <div className="bg-white mt-4 p-4 sm:p-5 rounded-xl shadow-md">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="font-semibold text-base sm:text-lg text-gray-900">
 					Trending Expert Opinion
 				</h2>
-			</div>
-			<div className="space-y-4">
-				{trendingExpertOpinions.map((post, index) => (
+        <Link
+					href="/influencer-comment/Expert Opinion"
+					className="text-indigo-600 text-xs sm:text-sm font-medium hover:underline"
+				>
+					VIEW MORE →
+				</Link>
+      </div>
+      <div className="space-y-3">
+        {trendingExpertOpinions?.length > 0 ? (
+          trendingExpertOpinions.map((post, index) => (
 					<div
 						key={`opinion-${post.postId}-${index}`}
 						className="block border-b border-dashed border-gray-300 pb-3 last:border-none hover:text-indigo-600 transition-colors"
@@ -29,19 +34,13 @@ const TrendingExpertOpinion = ({ trendingExpertOpinions }) => {
 							</h3>
 						</Link>
 					</div>
-				))}
-			</div>
-
-			<div className="mt-4 text-right">
-				<Link
-					href="/influencer-comment/Expert Opinion"
-					className="text-indigo-600 text-xs sm:text-sm font-medium hover:underline"
-				>
-					VIEW MORE →
-				</Link>
-			</div>
-		</div>
-	);
+				))
+        ) : (
+          <p className="text-gray-600 text-sm">Trending Expert Opinion will be Available Soon.</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default TrendingExpertOpinion;
+export default TrendingThemes;
